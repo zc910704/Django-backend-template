53cb7ce17aedefe639271ed2350ce731dbb62349
zc910704 <329188614@qq.com>
2018-11-16 18:57:03 +0800

    first commit

---
+++ login/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.contrib.auth.models import User 
+# Create your models here.
+ 
+class Extend_User(models.Model):
+    '''拓展默认用户'''
+
+    gender = (
+        ('male','男'),
+        ('none','未知'),
+        ('female','女'),
+    )
+
+    role = (
+        ('admin','admin'),
+        ('user','user')
+    )
+
+    real_name = models.CharField(max_length=128, blank=True)
+    avatar = models.CharField(max_length=128, blank=True)
+    roles = models.CharField(max_length=32,choices=role,default='admin')
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    sex = models.CharField(max_length=32,choices=gender,default='未知')
+
+ 
+    def __str__(self):
+        return self.real_name
+ 
+    class Meta:
+        ordering = ['user']
+        verbose_name = '用户'
+        verbose_name_plural = '用户'
+
+        """ 
+        各字段含义：
+            name为真实名字，最长不超过128个字符；
+            性别使用了一个choice，只能选择男或者女，默认为男；
+            使用__str__帮助人性化显示对象信息；
+            元数据里定义显示顺序；
+         """
---
+++ login/views.py
@@ -0,0 +1,79 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import JsonResponse
+# auth
+from django.contrib.auth import authenticate, login,logout
+from django.contrib.auth.models import Permission, User
+from django.contrib.contenttypes.models import ContentType
+# Extend_user
+from . import models
+# JSON
+import json
+# Create your views here.
+
+response_dict = {}
+token_dict = {}
+
+def dict_constructer( **kwargs ):
+    return kwargs
+
+def user_login(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        if username:
+            password = request.POST.get('password')
+        else:
+            # 当前端以payload形式发送JSON时，无法从POST方法取到值时，反序列化json
+            request_payload_obj = json.loads(request.body)
+            username = request_payload_obj['username']
+            password = request_payload_obj['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            response_dict["code"] = 20000
+            token_dict["token"] = username
+            response_dict["data"] = token_dict
+            print(response_dict)
+            return JsonResponse(response_dict)
+        else:
+            response_dict["code"] = 50008
+            token_dict["token"] = username
+            response_dict["data"] = token_dict
+            print(response_dict)
+            return JsonResponse(response_dict)
+
+
+def getInfo(request):
+    if request:
+        username = request.GET.get('token')
+        user = User.objects.get(username=username)
+        if user:
+            data_dict = {}
+            response_dict = {}
+            ext_user = models.Extend_User.objects.get(user=user)
+
+            response_dict["code"] = 20000
+
+            data_dict['username'] = username
+            data_dict['avatar'] = ext_user.avatar
+            data_dict['roles'] = ['admin']
+            response_dict["data"] = data_dict
+            response = JsonResponse(response_dict,json_dumps_params={'ensure_ascii':False})
+            return response
+
+
+def user_logout(request):
+  logout(request)
+  response_dict = {}
+  response_dict['code']= 20000
+  response_dict['data']= 'success'
+  return JsonResponse(response_dict)
+
+
+
+#https://www.cnblogs.com/wf-skylark/p/9317096.html
+#ensure_ascii是false的时候，可以返回非ASCII码的值，否则就会被JSON转义。
+#response对象
+#JSON序列化str = json.dumps(j)
+#https://docs.djangoproject.com/en/2.1/ref/request-response/
+
