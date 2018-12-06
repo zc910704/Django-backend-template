ce18cbf52d9d51b149b677b0df4c7e60d9aaef25
zc910704 <329188614@qq.com>
2018-12-06 19:32:01 +0800

    完成了投标试算的接口

--- compute/urls.py
+++ compute/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import auto_complete
+from .views import auto_complete, history_check
 
 urlpatterns = [
-    path('company/name', auto_complete)  # api: compute/company/name
+    path('company/name', auto_complete),  # api: compute/company/name
+    path('company/price/history', history_check)  # api: compute/company/price/history
 ]
