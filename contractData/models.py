fa04a9695a035bd39844a12cbb842bcf32fd125d
zc910704 <329188614@qq.com>
2018-12-31 23:42:20 +0800

    test

--- contractData/models.py
+++ contractData/models.py
@@ -43,3 +43,27 @@ class ContractPrice(models.Model):
     
     class Meta:
         ordering = ['contract']
+
+
+class BidPrice(models.Model):
+    """
+    detail price of the Contract
+    """
+    contract = models.ForeignKey(Contract, verbose_name='所属投标', null=True, on_delete=models.CASCADE)
+    classify = models.CharField(verbose_name='分类', max_length=128, null=True)
+    itemId = models.IntegerField(verbose_name='项目编码', null=True)
+    item = models.CharField(verbose_name='清单名称', max_length=128, null=True)
+    feature = models.TextField(verbose_name='项目特征', null=True)
+    description = models.TextField(verbose_name='项目内容', null=True)
+    unit = models.CharField(verbose_name='单位', max_length=12, null=True)
+    count = models.IntegerField(verbose_name='工程量', null=True)
+    priceNet = models.FloatField(verbose_name='不含税价', null=True)
+    taxRate = models.FloatField(verbose_name='税率', null=True, default=10.0)
+    priceTaxed = models.FloatField(verbose_name='含税价', null=True)
+    comment = models.CharField(verbose_name='备注', max_length=256, null=True)
+
+    def __str__(self):
+        return self.item
+
+    class Meta:
+        ordering = ['contract']
--- contractData/views.py
+++ contractData/views.py
@@ -1,5 +1,5 @@
 from django.http import JsonResponse
-from .models import ContractPrice
+from .models import ContractPrice, BidPrice
 import json
 # Create your views here.
 from django.contrib.auth.decorators import login_required
@@ -11,14 +11,31 @@ def price_detail_info(request):
     """
     if request.method == 'POST':
         keyword = request.POST.get('keyword')
+        database_set = request.POST.get('database')
         if not keyword:
             keyword = json.loads(request.body)['keyword']
+            database_set = json.loads(request.body)['database']
         print(keyword)
-        if keyword:
-            query_set = ContractPrice.objects.filter(item__contains=keyword) \
+    if keyword:
+        query_set_list = None
+        if database_set == 0 or not database_set:
+            # 0 means none database selected
+            pass
+        elif database_set == 1 or database_set - 3 == 1:
+            # 1 means the Contract database selected
+            query_set_list += ContractPrice.objects.filter(item__contains=keyword) \
                 .values('contract__contractName', 'contract__contractDate', 'item',
                         'feature', 'description', 'unit', 'priceNet',
                         'priceTaxed', 'classify', 'comment')
-            return JsonResponse({'code': 20000, 'data': list(query_set)})
+        elif database_set == 3 or database_set - 1 == 3:
+            # 3 means the Bid database selected
+            query_set_list += BidPrice.objects.filter(item__contains=keyword) \
+                .values('contract__contractName', 'contract__contractDate', 'item',
+                        'feature', 'description', 'unit', 'priceNet',
+                        'priceTaxed', 'classify', 'comment')
+        if len(query_set_list) == 2:
+            query_set = query_set_list[0] | query_set_list[1]
         else:
-            return JsonResponse({'code': 20000, 'data': None})
+            query_set = query_set_list
+
+        return JsonResponse({'code': 20000, 'data': list(query_set)})
